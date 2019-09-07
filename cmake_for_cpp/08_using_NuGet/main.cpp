6b99f8169577023ef6af1c6403558fd1b243071d
Pvs Phaneendra <Phaneendra.Paluri@infineon.com>
2019-09-07 16:38:05 +0530

    Deleting all sources and original documentation files

--- cmake_for_cpp/08_using_NuGet/main.cpp
+++
@@ -1,9 +0,0 @@
-#include <iostream>
-
-#include "systemc.h"
-
-int sc_main(int sc_argc, char * sc_argv[])
-{
-  std::cout << "\nRunning this SystemC demo example by using Pvs.SoftTools.SystemC NuGet package" << std::endl;
-  return 0;
-}
