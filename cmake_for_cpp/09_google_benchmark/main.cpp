b58a1ee6fdbf18e228ab3903423f0a7f5568ad30
Pvs Phaneendra <Phaneendra.Paluri@infineon.com>
2022-01-31 02:33:34 +0530

    Adding template project to work with google benchmark

---
+++ cmake_for_cpp/09_google_benchmark/main.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+#include <benchmark/benchmark.h>
+
+int main(int argc, char * argv[])
+{
+  std::cout << "\nRunning Google Benchmarking from NuGet package" << std::endl;
+  return 0;
+}
